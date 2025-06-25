e079064f2c427cabdb41b942a42e1fce37be58dc
Joel Kim <joelminchankim@gmail.com>
2025-06-25 00:37:25 -0400

    feat: Add navigation between login and signup screens

--- Momentum/Momentum/Views/LoginView.swift
+++ Momentum/Momentum/Views/LoginView.swift
@@ -5,43 +5,48 @@ struct LoginView: View {
     @State private var password = ""
 
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Momentum")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .padding(.bottom, 40)
+        NavigationStack {
+            VStack(spacing: 20) {
+                Text("Momentum")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.bottom, 40)
 
-            TextField("Email", text: $email)
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(8)
-                .keyboardType(.emailAddress)
-                .autocapitalization(.none)
-
-            SecureField("Password", text: $password)
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(8)
-                .textContentType(.newPassword)
-
-            Button(action: {
-                print("Login tapped. Email: \(email)")
-            }) {
-                Text("Login")
-                    .font(.headline)
-                    .foregroundColor(.white)
+                TextField("Email", text: $email)
                     .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
+                    .background(Color(.systemGray6))
                     .cornerRadius(8)
-            }
+                    .keyboardType(.emailAddress)
+                    .autocapitalization(.none)
 
-            Text("Don't have an account? Sign Up")
+                SecureField("Password", text: $password)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                    .textContentType(.newPassword)
+
+                Button(action: {
+                    print("Login tapped. Email: \(email)")
+                }) {
+                    Text("Login")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                }
+                
+                NavigationLink("Don't have an account? Sign Up") {
+                    SignupView()
+                }
                 .padding(.top)
 
-            Spacer()
+                Spacer()
+            }
+            .padding()
+            .navigationBarHidden(true)
         }
-        .padding()
     }
 }
 
